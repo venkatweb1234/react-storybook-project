97d26496073c91715309189cbd406a72215db1ab
Venkatesh Venkatesh <venkatesh.venkatesh@optus.com.au>
2021-06-09 18:35:23 +1000

    Args

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Button from './Button';
 export default{
     title:'Form/control/Button',
-    component:Button
+    component:Button,
+    args:{
+        children:'Button'
+    }
 }
 
 export const Primary = () =><Button variant="primary">Primary</Button>
@@ -10,4 +13,22 @@ export const Secondary = () =><Button variant="secondary">Secondary</Button>
 export const Success = () =><Button variant="success">Success</Button>
 export const Danger = () =><Button variant="danger">Danger</Button>
 
-Primary.storyName = 'Primary Venkat'
+const Template = args =><Button {...args} />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+    variant: 'primary',
+    //children: 'Primary Args'
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+    ...PrimaryA.args,
+    //children: 'Long Primary Args'
+}
+
+export const SecondaryA = Template.bind({})
+SecondaryA.args ={
+    variant: 'secondary',
+    //children: 'Secondary Args'
+}
