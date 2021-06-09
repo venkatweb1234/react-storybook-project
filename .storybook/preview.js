067802eb2c1dd6f5208e1697399facb9dfa01fd1
Venkatesh Venkatesh <venkatesh.venkatesh@optus.com.au>
2021-06-09 20:31:37 +1000

    DecotratorS

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
 
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import Center from '../src/components/Center/Center-component';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -12,3 +15,5 @@ export const parameters = {
       a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 }
+
+addDecorator(story =><Center>{story()}</Center>)
---
+++ src/components/Center/Center-component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Center-style.scss';
+const Center = props =>{
+    return <div className="center">{props.children}</div>
+}
+
+export default Center;
