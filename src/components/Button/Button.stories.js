d01cf56172b75d9820db1c2676a8bf38f6dab18a
Venkatesh Venkatesh <venkatesh.venkatesh@optus.com.au>
2021-06-09 15:19:13 +1000

    Renaming and Sorting Stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './Button';
 export default{
-    title:'Form/Button',
+    title:'Form/control/Button',
     component:Button
 }
 
@@ -9,3 +9,5 @@ export const Primary = () =><Button variant="primary">Primary</Button>
 export const Secondary = () =><Button variant="secondary">Secondary</Button>
 export const Success = () =><Button variant="success">Success</Button>
 export const Danger = () =><Button variant="danger">Danger</Button>
+
+Primary.storyName = 'Primary Venkat'
